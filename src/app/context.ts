b9f1acad3419e5d2369e6db8b8fab8253e69df1a
dayrain <blackwaterbread@gmail.com>
2022-09-11 03:03:11 +0900

    improvement context definition

--- src/app/context.ts
+++ src/app/context.ts
@@ -3,7 +3,7 @@ import Store from 'electron-store';
 
 const store = new Store();
 
-contextBridge.exposeInMainWorld('Application', {
+const contextApplication = {
     versions: {
         node: () => process.versions.node,
         chrome: () => process.versions.chrome,
@@ -12,10 +12,16 @@ contextBridge.exposeInMainWorld('Application', {
     setAppSize: (width: number, height: number) => { ipcRenderer.send('setSize', width, height) },
     setAppResizable: (resizable: boolean) => { ipcRenderer.send('setResizable', resizable); },
     isDevelopment: () => process.env.NODE_ENV === 'development'
-});
+}
 
-contextBridge.exposeInMainWorld('Store', {
+const contextStore = {
     get: (key: string) => store.get(key),
     set: (key: string, value: number | string | object) => store.set(key, value),
     delete: (key: string) => store.delete(key),
-});
+}
+
+contextBridge.exposeInMainWorld('Application', contextApplication);
+contextBridge.exposeInMainWorld('Store', contextStore);
+
+export const typeContextApplication = typeof contextApplication;
+export const typeContextStore = typeof contextStore;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,21 +1,10 @@
 /// <reference types="react-scripts" />
 
+import { typeContextApplication, typeContextStore } from 'app/context';
+
 declare global {
     interface Window {
-        Application: {
-            versions: {
-                node: () => string,
-                chrome: () => string,
-                electron: () => string,
-            },
-            setAppSize: (width: number, height: number) => void,
-            setAppResizable: (resizable: boolean) => void,
-            isDevelopment: () => boolean
-        },
-        Store: {
-            get: (key: string) => any,
-            set: (key: string, value: number | string | object) => void,
-            delete: (key: string) => void,
-        }
+        Application: typeContextApplication,
+        Store: typeContextStore
     }
 }
